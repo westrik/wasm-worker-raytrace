6ea27acbc98bc8e48b51c9673b44533444a8f722
Matthew Westrik <m@ttwestrik.com>
2019-08-02 00:52:53 -0700

    add wasm-pack test

--- tests/web.rs
+++ tests/web.rs
@@ -3,10 +3,23 @@
 #![cfg(target_arch = "wasm32")]
 
 use wasm_bindgen_test::*;
+use rampage::rampage::{render_scene, RenderConfig};
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn pass() {
     assert_eq!(1 + 1, 2);
+}
+
+#[wasm_bindgen_test]
+fn render() {
+    let render_config = RenderConfig {
+        num_threads: 1,
+        num_iterations: 100,
+        max_depth: 60,
+        dimensions: (400, 300),
+    };
+
+    let _rendered_scene = render_scene("random_spheres", render_config);
 }
