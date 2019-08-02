1c70300ea59b2f495978360f24394bc66fc7c138
Matthew Westrik <m@ttwestrik.com>
2019-08-02 00:35:09 -0700

    use wee_alloc for more pixels without crashing

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,17 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
+extern crate wee_alloc;
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[wasm_bindgen]
 pub fn greet() -> String {
     let render_config = RenderConfig {
         num_threads: 1,
         num_iterations: 40,
         max_depth: 20,
-        dimensions: (300, 250),
+        dimensions: (400, 250),
     };
     
     let _rendered_scene = render_scene("random_spheres", render_config);
